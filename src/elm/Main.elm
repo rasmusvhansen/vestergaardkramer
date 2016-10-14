918e32dc2fe0912e4129b7d075f7b9d7c91fe2ae
Rasmus Hansen <rasmusvhansen@gmail.com>
2016-10-14 22:48:59 +0200

    First mockup

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,9 +1,9 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
-import Time exposing (Time, second)
 
 
 main =
@@ -19,13 +19,27 @@ main =
 -- MODEL
 
 
+type alias Wish =
+    { person : String
+    , title : String
+    , description : String
+    , taken : Bool
+    }
+
+
 type alias Model =
-    Time
+    { wishes : List Wish
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( 0, Cmd.none )
+    ( Model
+        [ Wish "Rasmus" "Book" "Some book" False
+        , Wish "Rasmus" "En and" "En fin and" True
+        ]
+    , Cmd.none
+    )
 
 
 
@@ -33,27 +47,56 @@ init =
 
 
 type Msg
-    = Tick Time
+    = ToggleWish Wish Bool
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Tick newTime ->
-            ( newTime, Cmd.none )
+        ToggleWish wish isTaken ->
+            let
+                updateWish w =
+                    if w == wish then
+                        { w | taken = isTaken }
+                    else
+                        w
+            in
+                { model | wishes = List.map updateWish model.wishes }
+                    ! []
 
 
 
+-- case msg of
+--     Tick newTime ->
+--         ( newTime, Cmd.none )
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every second Tick
+    Sub.none
 
 
 
 -- VIEW
+
+
+viewWish : Wish -> Html Msg
+viewWish wish =
+    div [ class "wish" ]
+        [ label []
+            [ h5 []
+                [ input
+                    [ type' "checkbox"
+                    , checked wish.taken
+                    , onClick (ToggleWish wish (not wish.taken))
+                    ]
+                    []
+                , text (" " ++ wish.title)
+                ]
+            ]
+        , p [] [ text wish.description ]
+        ]
 
 
 view : Model -> Html Msg
@@ -61,13 +104,8 @@ view model =
     div [ class "container" ]
         [ div [ class "row" ]
             [ div [ class "one-half column" ]
-                [ h4 [] [ text "Some header of sorts" ]
-                , p []
-                    [ text "This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the "
-                    , a [ href "http://getskeleton.com" ] [ text "Skeleton" ]
-                    , text " and some more text"                    
-                    ]
-                 , p [] [text (toString model)]   
+                [ h1 [] [ text "Ønsker" ]
+                , div [] (List.map viewWish model.wishes)
                 ]
             ]
         ]
